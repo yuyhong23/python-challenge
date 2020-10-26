b1ad3402cf6722895802cba0e1f9277e069b1995
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2020-10-25 21:37:54 -0700

    Added some codes idea for calculating total, average change, greatest increase in profits and greatest decrease in profits in funtion. It doesn't work, but I will try to debug later to make the code runs.

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,8 +5,26 @@ financial_csv = os.path.join('..','Resources','budget_data.csv')
 
 month_count = 0
 
-print("Financial Analysis")
-print("------------------------------------------")
+def analysis(finanacial_data):
+    profit_losses = int(list(financial_data[1]))
+    date = str(financial_data[0])
+    date_list = int(list(financial_data[0]))
+
+    date_count = len(data_list)
+
+    total += profit_losses
+    net_total = total
+
+    average_change = net_total/date_count
+
+    Greatest_Increase = max(profit_losses)
+    Greatest_Decrease = min(profit_losses)
+
+    print(f'{date_count}')
+    print(f'{net_total}')
+    print(f'{average_change}')
+    print(f'{Greatest_Increase}')
+    print(f'{Greatest_Decrease}')
 
 with open(financial_csv, 'r') as csvfile:
 
@@ -16,5 +34,8 @@ with open(financial_csv, 'r') as csvfile:
 
     for row in csvreader:
         month_count += 1
-
+        analysis
+        
+print("Financial Analysis")
+print("------------------------------------------")
 print(f'Total Months: {month_count}')
