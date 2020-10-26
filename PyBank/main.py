ddc8c0bdff09fada8d89c95e581f1967f1d57450
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2020-10-26 16:58:52 -0700

    Added codes to get the net total amount of profit and losses.

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,27 +4,7 @@ import csv
 financial_csv = os.path.join('..','Resources','budget_data.csv')
 
 month_count = 0
-
-def analysis(finanacial_data):
-    profit_losses = int(list(financial_data[1]))
-    date = str(financial_data[0])
-    date_list = int(list(financial_data[0]))
-
-    date_count = len(data_list)
-
-    total += profit_losses
-    net_total = total
-
-    average_change = net_total/date_count
-
-    Greatest_Increase = max(profit_losses)
-    Greatest_Decrease = min(profit_losses)
-
-    print(f'{date_count}')
-    print(f'{net_total}')
-    print(f'{average_change}')
-    print(f'{Greatest_Increase}')
-    print(f'{Greatest_Decrease}')
+net_total_profit = 0
 
 with open(financial_csv, 'r') as csvfile:
 
@@ -34,8 +14,10 @@ with open(financial_csv, 'r') as csvfile:
 
     for row in csvreader:
         month_count += 1
-        analysis
+        profit = int(row[1])
+        net_total_profit = net_total_profit + profit
         
 print("Financial Analysis")
 print("------------------------------------------")
 print(f'Total Months: {month_count}')
+print(f'Total: ${net_total_profit:0,.0f}')
