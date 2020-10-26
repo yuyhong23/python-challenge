6e86276da10a6f743a44f007761b6d61372d2f3a
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2020-10-25 20:29:37 -0700

    Added codes to get the total number of months included in the dataset.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,20 @@
 import os
 import csv
 
-financial_data = os.path.join("..", "Resources", "budget_data.csv")
+financial_csv = os.path.join('..','Resources','budget_data.csv')
 
-with open(budget_data.csv, 'r') as csvfile:
+month_count = 0
+
+print("Financial Analysis")
+print("------------------------------------------")
+
+with open(financial_csv, 'r') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
     header = next(csvreader)
 
     for row in csvreader:
-        
+        month_count += 1
+
+print(f'Total Months: {month_count}')
