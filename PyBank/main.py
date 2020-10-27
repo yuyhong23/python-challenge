03548ae666f4d50a136f260323e2e59cfd4ffd33
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2020-10-27 14:40:54 -0700

    In the process of writing some scripts for getting the average of the changes in profit and losses column. The print last profit and list(change) are for me to make sure I am on the right track, will remove later once I have the complete scripts.

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,9 @@ financial_csv = os.path.join('Resources','budget_data.csv')
 
 month_count = 0
 net_total_profit = 0
+total_change = 0
+last_profit = 0
+change = []
 
 with open(financial_csv, 'r') as csvfile:
 
@@ -14,10 +17,22 @@ with open(financial_csv, 'r') as csvfile:
 
     for row in csvreader:
         month_count += 1
+
         profit = int(row[1])
+        month = row [0]
+        if month_count ==1:
+            last_profit = profit
+        else:
+            change.append(profit-last_profit)
+            last_profit = profit 
+           
+
         net_total_profit = net_total_profit + profit
         
 print("Financial Analysis")
 print("------------------------------------------")
 print(f'Total Months: {month_count}')
 print(f'Total: ${net_total_profit:0,.0f}')
+print(f'last profit: ${last_profit:0,.0f}')
+print(f'Average Change: ${total_change:0,.0f}')
+print(list(change))
