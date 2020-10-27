6dee41e101a4b30aa59b7ed9b0ed961232d6d1ed
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2020-10-27 15:00:46 -0700

    Wrote scripts for getting the average of the changes in profit losses.

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,9 +5,10 @@ financial_csv = os.path.join('Resources','budget_data.csv')
 
 month_count = 0
 net_total_profit = 0
-total_change = 0
 last_profit = 0
 change = []
+total_average_change = 0
+average_change_month_count = 0
 
 with open(financial_csv, 'r') as csvfile:
 
@@ -25,14 +26,20 @@ with open(financial_csv, 'r') as csvfile:
         else:
             change.append(profit-last_profit)
             last_profit = profit 
-           
+        
+        def average(change): 
+            total_change = 0
+            for value in change:
+                total_change += value
+            return total_change
 
         net_total_profit = net_total_profit + profit
-        
+    
+    average_change_month_count = month_count-1
+    total_average_change = average(change)/average_change_month_count 
+
 print("Financial Analysis")
 print("------------------------------------------")
 print(f'Total Months: {month_count}')
 print(f'Total: ${net_total_profit:0,.0f}')
-print(f'last profit: ${last_profit:0,.0f}')
-print(f'Average Change: ${total_change:0,.0f}')
-print(list(change))
+print(f'Average Change: ${total_average_change:0,.02f}')
