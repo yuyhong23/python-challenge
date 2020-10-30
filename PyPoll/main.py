1a8e09d13b49af262fc5ac77b5251912ebe0be2e
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2020-10-30 15:57:01 -0700

    Added a workable function that will return the unique candidate name in a list when called.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,20 @@ vote_count_csv = os.path.join('Resources','election_data.csv')
 
 # Declare the keys and their initial values
 voter_count = 0
+all_candidates = []
+
+# Function for getting each candidate's name without repeating
+def unique(candidate_list):
+    # Create a list for storing the unique candidate names
+    unique_list = []
+
+    # Read through the candidate list, if candidate name is not already in the list, add it
+    for candidates in candidate_list:
+        if candidates not in unique_list:
+            unique_list.append(candidates)
+
+    # End the function and return the result when called 
+    return unique_list
 
 # Read in the csv file
 with open(vote_count_csv, 'r') as csvfile:
@@ -21,8 +35,12 @@ with open(vote_count_csv, 'r') as csvfile:
     for row in csvreader:
         # Add 1 after each voter
         voter_count += 1
+        # Assign the candidate list to all_candidates
+        all_candidates.append(row[2])
+
 
 print("Election Results")
 print("--------------------------------")
 print(f'Total Votes: {voter_count:0,.0f}')# the :0,.0f is for converting the number to a easier read
 print("--------------------------------")
+print(unique(all_candidates))
